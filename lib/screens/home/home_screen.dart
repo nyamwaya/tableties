1c5ae8cd15e6ae7ea6d0a897bc0eef9a92411f84
nyamwaya <alecksonnyamwaya@gmail.com>
2024-08-21 22:41:12 -0500

    receiving the correct user args. database setup complete

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,21 +1,34 @@
-import 'package:TableTies/data_models/user_supabase.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:TableTies/data_models/user_supabase.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Retrieve the UserSupabase object from the arguments
-    final user = ModalRoute.of(context)?.settings.arguments as UserSupabase;
+    final user = ModalRoute.of(context)?.settings.arguments as UserSupabase?;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: Text('Welcome, ${user.firstName}!'),
-      ),
-    );
+    if (user == null) {
+      // Handle the case where no arguments were passed
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: const Center(
+          child: Text(
+              'Args were null. Type shit.'), // Display a message indicating null arguments
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: Center(
+          child: Text('Welcome, ${user.firstName}!'),
+        ),
+      );
+    }
   }
 }
