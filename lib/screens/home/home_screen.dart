f5067fab97bfe6975c76adb5ca3e55be75eeb03c
nyamwaya <alecksonnyamwaya@gmail.com>
2024-08-27 01:37:46 -0500

    header is stick and content is beign displayed beneith it

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:TableTies/widgets/header.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:TableTies/blocs/home/home_bloc.dart';
@@ -31,36 +32,44 @@ class HomePage extends StatelessWidget {
         }
       },
       builder: (context, state) {
-        if (state is HomeInitial || state is HomeSuccess) {
-          // Show loading indicator while fetching user or checking profile
-          return const Scaffold(
-              body: Center(child: CircularProgressIndicator()));
-        } else if (state is HomeProfileComplete) {
-          // Directly return the list page widget (assuming you have a ListPage widget)
-          return const Center(child: Text('List Page'));
-        } else if (state is HomeProfileIncomplete) {
-          // Directly return the complete profile widget (assuming you have a CompleteProfilePage widget)
-          return CompleteProfileWidget(missingIntrests: state.resource.data);
-        } else if (state is HomeFailure) {
-          return Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Error: ${state.resource.error}'),
-                  ElevatedButton(
-                    onPressed: () =>
-                        homeBloc.add(const CheckProfileCompletion()),
-                    child: const Text('Retry'),
-                  ),
-                ],
-              ),
-            ),
-          );
-        } else {
-          return Scaffold(body: Center(child: Text('Unknown state')));
-        }
+        return Scaffold(
+          appBar: AppBar(
+            title: AppHeader(),
+            automaticallyImplyLeading: false,
+          ),
+          body: _buildContent(context, state, homeBloc),
+        );
       },
     );
+  }
+}
+
+Widget _buildContent(BuildContext context, HomeState state, HomeBloc homeBloc) {
+  if (state is HomeInitial || state is HomeSuccess) {
+    // Show loading indicator while fetching user or checking profile
+    return const Scaffold(body: Center(child: CircularProgressIndicator()));
+  } else if (state is HomeProfileComplete) {
+    // Directly return the list page widget (assuming you have a ListPage widget)
+    return const Center(child: Text('List Page'));
+  } else if (state is HomeProfileIncomplete) {
+    // Directly return the complete profile widget (assuming you have a CompleteProfilePage widget)
+    return CompleteProfileWidget(missingIntrests: state.resource.data);
+  } else if (state is HomeFailure) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Error: ${state.resource.error}'),
+            ElevatedButton(
+              onPressed: () => homeBloc.add(const CheckProfileCompletion()),
+              child: const Text('Retry'),
+            ),
+          ],
+        ),
+      ),
+    );
+  } else {
+    return Scaffold(body: Center(child: Text('Unknown state')));
   }
 }
---
+++ lib/widgets/header.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppHeader extends StatelessWidget {
+  const AppHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      // Adjust padding as needed
+      color: Colors.white, // Or any desired background color
+      child: Row(
+        mainAxisAlignment:
+            MainAxisAlignment.spaceBetween, // Align items to left and right
+        children: [
+          Row(
+            // Group the pin and text together
+            children: [
+              Icon(Icons.location_on, color: Colors.red),
+              SizedBox(width: 8), // Add spacing between icon and text
+              Text('Select A City',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          Icon(Icons.notifications, color: Colors.orange),
+        ],
+      ),
+    );
+  }
+}
